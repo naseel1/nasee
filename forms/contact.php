09642ca38b2c5ffec2bb93d693358ad084501b8e
mincetech <naseelntm00@gmail.com>
2024-08-08 09:50:26 +0530

    nasee website form

--- forms/contact.php
+++ forms/contact.php
@@ -24,16 +24,5 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  $contact->smtp = array(
-    'host' => 'https://naseel1.github.io/nasee/',
-    'username' => 'naseel1',
-    'password' => '0055',
-    'port' => '5432'
-  );
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
